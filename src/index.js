df79ce8957e9faff17d235b8452fd6e49ca18ab3
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2020-03-17 14:11:44 +0300

    docs: update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array==null || array.length==0) return 0;
+  else{
+  array.sort(function(a, b) {
+  return a - b;});
+  return array[0];}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array==null || array.length==0) return 0;
+  else{
+  array.sort(function(a, b) {
+  return b - a;});
+  return array[0];}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array==null || array.length==0) return 0;
+  else{
+  let sum=0;
+  for (let i=0;i<array.length;i++)
+  {
+      sum+=array[i]/array.length;
+  }  
+  return (Math.round(sum*100)/100);}
 }
